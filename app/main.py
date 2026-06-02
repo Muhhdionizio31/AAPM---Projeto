eb4f1b4d326c03023ae2eb4b63480d7feef33a6d
luis <luiseduardo493333@gmail.com>
2026-06-02 14:29:29 -0300

    Implement PDV functionality with venda and item_venda models, and update main application routing

--- app/main.py
+++ app/main.py
@@ -11,6 +11,7 @@ from app.controllers import produto_controller
 from app.controllers import variacao_controller
 from app.controllers import movimentacao_controller
 from app.controllers import cliente_controller
+from app.controllers import pdv_controllers
 
 
 from dotenv import load_dotenv
@@ -34,6 +35,7 @@ app.include_router(produto_controller.router)
 app.include_router(variacao_controller.router)
 app.include_router(movimentacao_controller.router)
 app.include_router(cliente_controller.router)
+app.include_router(pdv_controllers.router)
 
 
 
