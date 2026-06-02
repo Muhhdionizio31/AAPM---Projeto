565ae91aa6f6018ff207c381d781abd4b36a5408
luis <luiseduardo493333@gmail.com>
2026-06-02 14:02:05 -0300

    adicionando cliente no models

--- app/main.py
+++ app/main.py
@@ -10,6 +10,7 @@ from app.controllers import categoria_controller
 from app.controllers import produto_controller
 from app.controllers import variacao_controller
 from app.controllers import movimentacao_controller
+from app.controllers import cliente_controller
 
 from dotenv import load_dotenv
 import os
@@ -31,6 +32,7 @@ app.include_router(categoria_controller.router)
 app.include_router(produto_controller.router)
 app.include_router(variacao_controller.router)
 app.include_router(movimentacao_controller.router)
+app.include_router(cliente_controller.router)
 
 
 #Rota para a página inicial
