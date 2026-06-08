b8a4bdba3e89ec77e662d2990a3667f57ab58e5a
Davi Tomé <dtomedev08@gmail.com>
2026-06-08 16:50:01 -0300

    Adiciona lógica de controle de estoque e vendas no painel; atualiza template para exibir dados dinâmicos de produtos, vendas e receita

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, RedirectResponse
 from app.auth import get_usuario_logado, get_usuario_opcional
+from sqlalchemy import func
+from datetime import datetime
 
 from app.controllers import auth_controller
 from app.controllers import admin_controller
@@ -21,6 +23,7 @@ from sqlalchemy.orm import Session
 
 from app.models.cliente import Cliente
 from app.models.produto import Produto
+from app.models.venda import Venda, ItemVenda
 
 
 load_dotenv()
@@ -46,6 +49,7 @@ app.include_router(pdv_controllers.router)
 @app.get("/")
 def home(
     request: Request,
+    db: Session = Depends(get_db),
     usuario = Depends(get_usuario_opcional)
     ):
     
@@ -56,12 +60,44 @@ def home(
             "site/index.html",
             {"request": request}       
         )
+    
+    total_produtos = db.query(func.count(Produto.estoque_atual)).scalar() or 0
+    estoque_critico = db.query(Produto).filter(Produto.estoque_atual <= 7).count()
+    produtos_recentes = db.query(Produto).order_by(Produto.id.desc()).limit(5).all()
+    receita_total = db.query(func.sum(Produto.estoque_atual * Produto.preco)).scalar() or 0
 
-    #Logado
+    mes_atual = datetime.now().month
+    ano_atual = datetime.now().year
+    total_vendas_mes = db.query(Venda).filter(
+        func.extract('month', Venda.criado_em) == mes_atual,
+        func.extract('year', Venda.criado_em) == ano_atual
+    ).count()
+    
+    ultimas_vendas = (db.query(ItemVenda).join(Venda).order_by(Venda.criado_em.desc()).limit(5).all())
+
+    # ── 4. GRÁFICO: VENDAS POR MÊS ──
+    vendas_por_mes_dados = db.query(func.extract('month', Venda.criado_em).label('mes'),
+        func.count(Venda.id).label('total')).filter(func.extract('year', Venda.criado_em) == ano_atual).group_by('mes').all()
+
+    vendas_mensais_lista = [0] * 12
+    for dado in vendas_por_mes_dados:
+        if dado.mes:
+            vendas_mensais_lista[int(dado.mes) - 1] = dado.total
+    
     return templates.TemplateResponse(
         request,
         "painel/index.html",
-        {"request": request, "usuario": usuario}       
+        {
+            "request": request, 
+            "usuario": usuario,
+            "produtos_em_estoque": total_produtos,
+            "estoque_critico": estoque_critico,
+            "produtos_recentes": produtos_recentes,
+            "receita_total": receita_total,
+            "total_vendas_mes": total_vendas_mes,
+            "ultimas_vendas": ultimas_vendas,
+            "vendas_mensais_lista": vendas_mensais_lista
+        }       
     )
 
 # Rota para o horário de atendimento
